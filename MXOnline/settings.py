16ee26d2be5483ec3c4d7ba28ad7047bb5d1b49a
mst <>
2018-03-17 22:47:45 +0800

    mac 推送

--- MXOnline/settings.py
+++ MXOnline/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'courses',
     'organization',
     'operation',
+    'xadmin',
+    'crispy_forms',
 ]
 
 # override the default users Model
@@ -112,15 +114,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- apps/users/models.py
+++ apps/users/models.py
@@ -15,7 +15,7 @@ class UserProfile(AbstractUser):
 
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
-    gender = models.CharField(max_length=10, choices=GENDER_ITEMS, default=u"男")
+    gender = models.CharField(max_length=10, choices=GENDER_ITEMS, default=u"female")
     address = models.CharField(max_length=100, default=u"")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y、%m", default="image/default")
