f45f142c6fac63a0bec9be57c6fa882cf73e3273
mst <>
2018-03-19 18:15:35 +0800

    实现用户注册模块1

--- apps/users/form.py
+++ apps/users/form.py
@@ -2,9 +2,16 @@
 __author__ = 'cao.yh'
 __date__ = '2018/3/19 上午10:25'
 from django import forms
+from captcha.fields import CaptchaField
 
 
 class LoginForm(forms.Form):
     # username和password必须同HTML的form中name字段相同的值
     username = forms.CharField(required=True)
     password = forms.CharField(required=True, min_length=5)
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(required=True, min_length=5)
+    captcha = CaptchaField(error_messages={"invalid": "验证码错误"})
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.hashers import make_password
 from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 # Create your views here.
 from django.views.generic.base import View
 
-from users.form import LoginForm
+from users.form import LoginForm, RegisterForm
 from users.models import UserProfile
 
 
@@ -23,6 +24,7 @@ class CustomBackend(ModelBackend):
             return None
 
 
+# 用户登录模块
 class LoginView(View):
     def get(self, request):
         return render(request, "login.html", {})
@@ -40,3 +42,22 @@ class LoginView(View):
                 return render(request, "login.html", {"msg": "用户名或密码错误"})
         else:
             return render(request, "login.html", {"login_form": login_form})
+
+
+# 用户注册模块
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, "register.html", {'register_form': register_form})
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name = request.POST.get("username", "")
+            password = request.POST.get("password", "")
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            user_profile.password = make_password(password)
+            user_profile.save()
+
